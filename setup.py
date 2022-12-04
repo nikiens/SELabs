05510479ea1f5d1c161dfb865945558718e09146
Dimitri Anikin <dimitri.anikin@inboc.eu>
2022-12-04 13:50:50 +0300

    Add tokenization

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+import nltk
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+class PostInstallCommand(install):
+    """Post-installation for development mode."""
+    def run(self):
+        install.run(self)
+        os.system("python3 -m spacy download en_core_web_sm")
+
+setup(
+	name="preprocessor",
+	version="0.1",
+	packages=find_packages(include=['preprocessor', 'preprocessor.*']),
+        cmdclass={
+            'install': PostInstallCommand,
+    }
+)
